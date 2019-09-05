2af7a1f45f117c09bad27b7d794b5007f73f382c
DanielSaenz2001 <daniel.saenz@upeu.edu.pe>
2019-09-05 06:48:23 -0500

    avance

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './views/home/home.component';
+import { ProductosComponent } from './views/productos/productos.component';
+import { CategoriasComponent } from './views/categorias/categorias.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'productos',component:ProductosComponent},
+  {path:'categorias',component:CategoriasComponent},
+  {path:'**',component:PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ProductosComponent } from './productos/productos.component';
-import { CategoriasComponent } from './categorias/categorias.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'Categorias', component: CategoriasComponent },
-  { path: 'Productos', component: ProductosComponent },
-  //{ path: '**', component: PageNotFoundComponent }
-];
+import { HomeComponent } from './views/home/home.component';
+import { ProductosComponent } from './views/productos/productos.component';
+import { CategoriasComponent } from './views/categorias/categorias.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
+
 
 @NgModule({
   declarations: [
@@ -25,10 +19,8 @@ const appRoutes: Routes = [
     PageNotFoundComponent
   ],
   imports: [
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    )
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
