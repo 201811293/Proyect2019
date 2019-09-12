ad75d7f43fe7e08d7e61f3048a7c6e7d8988d052
DanielSaenz2001 <daniel.saenz@upeu.edu.pe>
2019-09-11 21:10:48 -0500

    otro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,23 @@ import { HomeComponent } from './views/home/home.component';
 import { ProductosComponent } from './views/productos/productos.component';
 import { CategoriasComponent } from './views/categorias/categorias.component';
 import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
+import { RegisterComponent } from './user/register/register.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { ListWallpaperComponent } from './admin/list-wallpaper/list-wallpaper.component';
+import { DetailsWallpapperComponent } from './views/details-wallpapper/details-wallpapper.component';
+import { LoginComponent } from './user/login/login.component';
+
+
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'productos',component:ProductosComponent},
   {path:'categorias',component:CategoriasComponent},
+  {path:'user/register',component:RegisterComponent},
+  {path:'user/profile',component:ProfileComponent},
+  {path:'user/login',component:LoginComponent},
+  {path:'admin/list-wall',component:ListWallpaperComponent},
+  {path:'wallpaper:/id',component:DetailsWallpapperComponent},
   {path:'**',component:PageNotFoundComponent},
 ];
 
